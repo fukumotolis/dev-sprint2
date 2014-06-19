988b2c5c03f4b70d66dd8e8824753a3e2cb8816b
Risa <selfrising@gmail.com>
2014-06-18 21:46:08 -0500

    lab 4 beginnings

--- base_app/base_app.py
+++ base_app/base_app.py
@@ -1,21 +1,36 @@
-import flask, flask.views
-import os
+from flask import Flask, flash
+import flask.views
+import os 
 
-app = flask.Flask(__name__)
+app = flask.Flask(__name__) # i don't understand name
 
-app.secret_key = "bacon"
+app.secret_key = "lettuce"
 
-class View(flask.views.MethodView):
+
+class Main(flask.views.MethodView):
+	def get(self):
+		pass
+
+	def post(self):
+		pass
+
+
+class Remote(flask.views.MethodView):	# what's a class?
 	def get(self):
 		return flask.render_template('index.html')
 
 	def post(self):
-		result = eval (flask.request.form['expression'])
+		result = eval(flask.request.form['expression'])
 		flask.flash(result)
 		return self.get()
-		
 
-app.add_url_rule('/', view_func=View.as_view('main'), methods=['GET', 'POST'])
+add.app_url_rule('/',
+	view_func=Main.as_view('index'),
+	methods=['GET', 'POST'])
 
+app.add_url_rule('/remote/', 
+	view_func=Remote.as_view('main'), 
+	methods=['GET', 'POST'])	
+# i have no idea what's happening
 app.debug = True
-app.run()
+app.run()	# is this invoking the class?
