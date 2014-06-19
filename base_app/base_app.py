fb7a4e0734fff74722168206c88c0b92f0584338
Risa <lisaffang@gmail.com>
2014-06-19 18:19:26 -0500

    lab 4 attempt 2

--- base_app/base_app.py
+++ base_app/base_app.py
@@ -1,36 +1,60 @@
-from flask import Flask, flash
-import flask.views
-import os 
-
-app = flask.Flask(__name__) # i don't understand name
-
+import flask, flask.views
+import os
+import functools
+app = flask.Flask(__name__)
+# Don't do this!
 app.secret_key = "lettuce"
 
+users = {'lisa':'lettuce'}
 
 class Main(flask.views.MethodView):
-	def get(self):
-		pass
+    def get(self):
+        return flask.render_template('index.html')
+    
+    def post(self):
+        if 'logout' in flask.request.form:
+            flask.session.pop('username', None)
+            return flask.redirect(flask.url_for('index'))
+        required = ['username', 'passwd']
+        for r in required:													# what does this for loop do? how is it counting?
+            if r not in flask.request.form:
+                flask.flash("Error: {0} is required.".format(r))
+                return flask.redirect(flask.url_for('index'))
+        username = flask.request.form['username']
+        passwd = flask.request.form['passwd']
+        if username in users and users[username] == passwd:
+            flask.session['username'] = username
+        else:
+            flask.flash("Username doesn't exist or incorrect password")
+        return flask.redirect(flask.url_for('index'))
 
-	def post(self):
-		pass
+def login_required(method):													# why is this here?
+    @functools.wraps(method)												# what is this doing?
+    def wrapper(*args, **kwargs):
+        if 'username' in flask.session:
+            return method(*args, **kwargs)
+        else:
+            flask.flash("A login is required to see the page!")
+            return flask.redirect(flask.url_for('index'))
+    return wrapper
 
+class Remote(flask.views.MethodView):
+    @login_required
+    def get(self):
+        return flask.render_template('remote.html')
+        
+    @login_required
+    def post(self):
+        result = eval(flask.request.form['expression'])
+        flask.flash(result)
+        return flask.redirect(flask.url_for('remote'))
+    
+app.add_url_rule('/',
+                 view_func=Main.as_view('index'),
+                 methods=["GET", "POST"])
+app.add_url_rule('/remote/',
+                 view_func=Remote.as_view('remote'),
+                 methods=['GET', 'POST'])
 
-class Remote(flask.views.MethodView):	# what's a class?
-	def get(self):
-		return flask.render_template('index.html')
-
-	def post(self):
-		result = eval(flask.request.form['expression'])
-		flask.flash(result)
-		return self.get()
-
-add.app_url_rule('/',
-	view_func=Main.as_view('index'),
-	methods=['GET', 'POST'])
-
-app.add_url_rule('/remote/', 
-	view_func=Remote.as_view('main'), 
-	methods=['GET', 'POST'])	
-# i have no idea what's happening
 app.debug = True
-app.run()	# is this invoking the class?
+app.run()
